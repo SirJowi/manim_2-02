b9ffde3600cc07ac88adecca638065f2f3f7cef8
SirJowi <johann-sinner@web.de>
2023-04-14 17:57:25 +0200

    font is working now. kerning issues removed by using 10x the font size and downscaling it back to normal with scale(0.1)

--- title_gradient.py
+++ title_gradient.py
@@ -1,13 +1,8 @@
 from manim import *
 from datetime import *
-from manim_fonts import *
-
 
 class static_title(Scene):
     def construct(self):
-        with RegisterFont("Open Sans") as fonts:
-            Text.set_default(font=fonts[0])
-
         # background with blue gradient on the title-master
         background_gradient = ImageMobject("background_gradient.png")
         self.add(background_gradient)
@@ -19,15 +14,17 @@ class static_title(Scene):
         tud_logo.scale(0.42)
         self.add(tud_logo)
 
+        Text.set_default(font="Open Sans")
+
         # placeholder that allows to left align a VGroup (invisible dot, that has the correct x-coordinate)
         align_left = Text(".", fill_opacity=0)
         align_left.set_x(-6)
 
         # first textblock with name and institution
         name = Text("Johannes, Storm",
-                    font_size=16, weight=BOLD, fill_opacity=0.6)
+                    font_size=160, weight=BOLD, fill_opacity=0.6).scale(0.1)
         institute = Text("Institut für Statik und Dynamik",
-                         font_size=16, fill_opacity=0.6)
+                         font_size=160, fill_opacity=0.6).scale(0.1)
 
         # align text to the desired x-offset
         textblock_1 = VGroup(align_left, name, institute)
@@ -37,9 +34,9 @@ class static_title(Scene):
 
         # second textblock with title and subtitle
         title = Text("Titelmaster",
-                     font_size=32, weight=BOLD)
+                     font_size=320, weight=BOLD).scale(0.1)
         subtitle = Text("Titelzusatz hinzufügen",
-                        font_size=32)
+                        font_size=320).scale(0.1)
 
         textblock_2 = VGroup(align_left, title, subtitle)
         textblock_2.arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.3)
@@ -49,14 +46,9 @@ class static_title(Scene):
         # third textblock with date, location
         timestamp = datetime.today().strftime('%d. %B %Y')
         date = Text("Dresden // " + timestamp,
-                    font_size=16, fill_opacity=0.6)
+                    font_size=160, fill_opacity=0.6).scale(0.1)
 
         textblock_3 = VGroup(align_left, date)
         textblock_3.arrange(DOWN, center=False, aligned_edge=LEFT)
         textblock_3.set_y(-2.18)
         self.add(textblock_3)
-
-
-        with RegisterFont("Poppins") as fonts:
-            a = Text("Hello World", font=fonts[0])
-            self.play(Write(a))
