527b35b7766964a5ba4eec83c8efa48369404b33
SirJowi <johann-sinner@web.de>
2023-04-14 17:08:36 +0200

    adding all text blocks, aligning them based on the corporate design layout (only possible with eyeballing) automated the current date font not working

--- title_gradient.py
+++ title_gradient.py
@@ -1,8 +1,9 @@
 from manim import *
+from datetime import *
+
 
 class static_title(Scene):
     def construct(self):
-        register_font("Open_Sans/static/OpenSans/OpenSans-Bold.ttf")
         Text.set_default(font="Open Sans")
 
         # background with blue gradient on the title-master
@@ -16,17 +17,39 @@ class static_title(Scene):
         tud_logo.scale(0.42)
         self.add(tud_logo)
 
-        name = Text("Vorname, Name", font_size=16, font="OpenSans", weight=BOLD, fill_opacity=0.6)
-
-        institute = Text("Struktureinheit der TU Dresden", font_size=16, font="OpenSans", fill_opacity=0.6)
-
+        # placeholder that allows to left align a VGroup (invisible dot, that has the correct x-coordinate)
         align_left = Text(".", fill_opacity=0)
         align_left.set_x(-6)
 
-        textblock = VGroup(align_left, name, institute)
-        textblock.arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.1)
-        self.add(textblock)
+        # first textblock with name and institution
+        name = Text("Johannes, Storm",
+                    font_size=16, font="OpenSans", weight=BOLD, fill_opacity=0.6)
+        institute = Text("Institut für Statik und Dynamik",
+                         font_size=16, fill_opacity=0.6)
 
+        # align text to the desired x-offset
+        textblock_1 = VGroup(align_left, name, institute)
+        textblock_1.arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.1)
+        textblock_1.set_y(0.45)
+        self.add(textblock_1)
 
+        # second textblock with title and subtitle
+        title = Text("Titelmaster",
+                     font_size=32, weight=BOLD)
+        subtitle = Text("Titelzusatz hinzufügen",
+                        font_size=32)
 
+        textblock_2 = VGroup(align_left, title, subtitle)
+        textblock_2.arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.3)
+        textblock_2.set_y(-0.925)
+        self.add(textblock_2)
 
+        # third textblock with date, location
+        timestamp = datetime.today().strftime('%d. %B %Y')
+        date = Text("Dresden // " + timestamp,
+                    font_size=16, fill_opacity=0.6)
+
+        textblock_3 = VGroup(align_left, date)
+        textblock_3.arrange(DOWN, center=False, aligned_edge=LEFT)
+        textblock_3.set_y(-2.18)
+        self.add(textblock_3)
