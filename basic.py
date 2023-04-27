a5827827311af332efa6858432dab46dc7f0f954
SirJowi <johann-sinner@web.de>
2023-04-27 14:45:34 +0200

    add isd logo color

--- basic.py
+++ basic.py
@@ -1,13 +1,13 @@
 from manim import *
 from datetime import *
 
-
 class static_basic(Scene):
     def construct(self):
         tud_darkblue = "#00305e"
         tud_lightblue = "#006ab3"
         tud_grey = "#A6A6A6"
 
+        # white background basic-master with the correct resolution
         background = Rectangle(color=WHITE, fill_opacity=1.0, height=1920, width=1080, stroke_width=0)
         self.add(background)
 
@@ -24,6 +24,13 @@ class static_basic(Scene):
         ddc_logo.set_y(-3.55)
         ddc_logo.scale(0.21)
         self.add(ddc_logo)
+
+        # logo from isd in color
+        isd_logo_color = ImageMobject("logo_isd_bunt.png")
+        isd_logo_color.set_x(4.5)
+        isd_logo_color.set_y(-3.55)
+        isd_logo_color.scale(1.2)
+        self.add(isd_logo_color)
 
         # setting up default font
         Text.set_default(font="Open Sans")
@@ -50,10 +57,10 @@ class static_basic(Scene):
 
         # second textblock with page number
         page = Text("Folie",
-                     font_size=160, color="#727277").scale(0.05)
+                    font_size=160, color="#727277").scale(0.05)
         page.set_x(1.38)
         number = Text("Foliennummer",
-                     font_size=160, color="#727277").scale(0.05)
+                      font_size=160, color="#727277").scale(0.05)
         textblock_2 = VGroup(page, number)
         textblock_2.arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.05)
         textblock_2.set_y(-3.43)
@@ -61,7 +68,7 @@ class static_basic(Scene):
 
         # adding header
         header = Text("Überschrift hinzufügen",
-                     font_size=240, color=tud_darkblue, weight=BOLD).scale(0.1)
+                      font_size=240, color=tud_darkblue, weight=BOLD).scale(0.1)
         textblock_3 = VGroup(align_left, header)
         textblock_3.arrange(DOWN, center=False, aligned_edge=LEFT)
         textblock_3.set_y(3.55)
